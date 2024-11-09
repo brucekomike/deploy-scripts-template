5cb12af11e0a56388842bc5ea6953d749c9b6802
brucekomike <brucekomike@gmail.com>
2024-11-09 22:26:46 +0800

    add systemd

--- config.example.sh
+++ config.example.sh
@@ -5,3 +5,13 @@
 #  \___/|____/ 
 OS_name=ubuntu
 # currently only support ubuntu
+$init_system=systemd
+
+
+if [ "$init_system" = "systemd" ]; then
+  DAEMON_restart="systemctl restart "
+  DAEMON_reload="systemctl reload "
+  DAEMON_start="systemctl start "
+  DAMMON_stop="systemctl stop "
+fi
+
